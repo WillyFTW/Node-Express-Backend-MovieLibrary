b42afd7c7db5f87ef804c84a72af77caa907ddd6
Willy <willyca97@gmail.com>
2024-08-01 12:46:48 +0200

    fixed view engine to show make /home work again

--- startup/view.js
+++ startup/view.js
@@ -1,5 +1,6 @@
 module.exports = function(app) {
     //returning html markup with view engine pug which gets required indirectly via app.set
     app.set("view engine", "pug"); //probably not neccessary
-    app.set("views", "../views"); //This Line is not necessary because ./views is the default path for view engines.
+    //app.set("views", "../views"); //This Line is not necessary because ./views is the default path for view engines.
+    //Also this line somehow throws an error
 }
