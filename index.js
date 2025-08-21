0327acf4aca4af46f5f8c3b94d841cfa083cdf46
Willy <willyca97@gmail.com>
2025-08-22 01:58:45 +0200

    unrelevant changes of comments

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 var winston = require("winston");
 
-
 const app = express();
 //the required function gets returned and executed.
-require("./startup/logging")(); //logging is the first line so it can log following errors. 
+require("./startup/logging")(); //logging is the first line so it can log following errors.
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")(app);
@@ -12,13 +11,10 @@ require("./startup/validation")();
 require("./startup/view")(app);
 require("./startup/prod")(app);
 
-
-
 // listening on a Port
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
+const server = app.listen(port, () =>
+  winston.info(`Listening on port ${port}...`)
+);
 
 module.exports = server;
-
-
- 
